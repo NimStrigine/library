76cb59c61a18b455b4e910a612085e83ae15b987
Nim Strigine <nimstrigine@gmail.com>
2021-12-26 17:42:02 -0600

    Finish most functionality

--- scripts.js
+++ scripts.js
@@ -20,24 +20,14 @@ Book.prototype.info = function() {
   }
   return (this.title + "\r\n" + this.author + "\r\n" + this.pages + "\r\n" + readString);
 }
+// Change read status function
+Book.prototype.hasRead = function() {
+  this.read = !this.read;
+}
 
-const book1 = new Book("test1", "author1", 10, false);
-const book2 = new Book("test2", "author2", 20, false);
-myLibrary.push(book1);
-myLibrary.push(book2);
-
-// refreshLibrary();
-// console.log(library.children);
-// removeChildren();
-
-// const book3 = new Book("test3", "author3", 30, true);
-// myLibrary.push(book3);
-// refreshLibrary();
-// // console.log(library.children);
-
+// clear DOM of books
 function removeChildren() {
   let booklist = document.querySelectorAll(".card");
-  // console.log(booklist);
   for (let i = 0; i < booklist.length; i++) {
     booklist[i].remove();
   }
@@ -50,22 +40,18 @@ function refreshLibrary() {
     const removeBtn = document.createElement("button");
     removeBtn.innerText = "Remove";
     divBook.className = "card";
-    divBook.dataset.indexNumber = myLibrary.indexOf(book);
-    console.log(myLibrary.indexOf(book));
-    console.log(divBook);
+    divBook.setAttribute("id", myLibrary.indexOf(book));
     divBook.innerText = book.info();
     divBook.appendChild(removeBtn);
     library.appendChild(divBook);
     removeBtn.addEventListener("click", () => removeBook(removeBtn.parentNode));
-    // console.log(removeBtn.parentNode);
   }
 };
 
+// remove single book
 function removeBook(node) {
   myLibrary.splice(node.dataset.indexNumber, 1);
   node.parentNode.removeChild(node);
-  // console.log(myLibrary);
-  // console.log(library.children);
 }
 
 // bring up form for new book
@@ -73,19 +59,46 @@ addBook.addEventListener("click", () => {
   popup.style.visibility = "visible";
 });
 
-//save book info
+// save book info
 save.addEventListener("click", (e) => {
   e.preventDefault();
-  let title = document.getElementByName("#title").value;
-  const author = document.querySelector("#author").value;
-  const pages = document.querySelector("#pages").value;
-  const read = document.querySelector("#read").value;
+  let title = document.querySelector("#title");
+  let author = document.querySelector("#author");
+  let pages = document.querySelector("#pages");
+  let read = document.querySelector("#read");
 
-  const newBook = new Book(title, author, pages, read);
+  const newBook = new Book(title.value, author.value, pages.value, read.checked);
   myLibrary.push(newBook);
   refreshLibrary();
-  title.value = "";
-  author = "";
-  pages.value = "";
-  read.value = false;
+  clearForm();
 });
+
+// clear and rehide form
+function clearForm() {
+  let title = document.querySelector("#title");
+  let author = document.querySelector("#author");
+  let pages = document.querySelector("#pages");
+  let read = document.querySelector("#read");
+
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+  read.checked = false;
+  popup.style.visibility = "hidden";
+}
+
+
+// TEST STUFF
+// const book1 = new Book("test1", "author1", 10, false);
+// const book2 = new Book("test2", "author2", 20, false);
+// myLibrary.push(book1);
+// myLibrary.push(book2);
+
+// refreshLibrary();
+// console.log(library.children);
+// removeChildren();
+
+// const book3 = new Book("test3", "author3", 30, true);
+// myLibrary.push(book3);
+// refreshLibrary();
+// // console.log(library.children);
